5664fcd513f45633ccd90a08b7b8aed6b3338f8d
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-05 23:22:29 +0530

    feat: create  rearing record data class for representing records

---
+++ app/src/main/java/com/example/reshme_nammapride/data/local/entity/RearingRecord.kt
@@ -0,0 +1,15 @@
+package com.example.reshme_nammapride.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.reshme_nammapride.domain.model.InstarStage
+
+@Entity(tableName = "rearing_records")
+data class RearingRecord(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val batchId: Int, // Foreign Key link to Batch
+    val timestamp: Long,
+    val temperature: Float,
+    val humidity: Float,
+    val stage: InstarStage // We'll need a Converter for this!
+)
