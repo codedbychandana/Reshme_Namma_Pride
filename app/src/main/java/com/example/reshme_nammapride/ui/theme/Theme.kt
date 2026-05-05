ec335e468e32d76398b87ea171c159a6c1d0bf9f
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-05 19:28:27 +0530

    feat: define light and dark themes

--- app/src/main/java/com/example/reshme_nammapride/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reshme_nammapride/ui/theme/Theme.kt
@@ -12,40 +12,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = RawSilkGold,
+    secondary = SoftLeafGreen,
+    tertiary = SilkCream,
+    background = DeepCharcoal,
+    surface = DeepCharcoal,
+    onPrimary = Black,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = MulberryGreen,
+    secondary = SoftLeafGreen,
+    tertiary = RawSilkGold,
+    background = SilkCream,
+    surface = White,
+    onPrimary = White,
+    onBackground = DeepCharcoal,
+    onSurface = DeepCharcoal
 )
 
 @Composable
 fun ReshmeNammaPrideTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
