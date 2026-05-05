ea75aa88d9747ff033aa834223c95813364410e3
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-05 19:25:31 +0530

    feat: define colors for app

--- app/src/main/java/com/example/reshme_nammapride/ui/theme/Color.kt
+++ app/src/main/java/com/example/reshme_nammapride/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.reshme_nammapride.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Palette
+val MulberryGreen = Color(0xFF2D5A27)
+val SoftLeafGreen = Color(0xFF4CAF50)
+val SilkCream = Color(0xFFFFFDF5)
+val RawSilkGold = Color(0xFFFFD700)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Status Colors for the Climate Dial
+val AlertRed = Color(0xFFE53935)
+val WarningOrange = Color(0xFFFFB300)
+val SafeGreen = Color(0xFF43A047)
+
+// Neutral Colors
+val DeepCharcoal = Color(0xFF212121)
+val EarthyGray = Color(0xFF757575)
+val White = Color(0xFFFFFFFF)
