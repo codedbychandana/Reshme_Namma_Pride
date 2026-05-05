20d138f5caadf463f8fe304f36c3e3dee9c1cf86
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-05 19:35:20 +0530

    feat: create enum class for stages of silkworm

---
+++ app/src/main/java/com/example/reshme_nammapride/domain/model/InstarStage.kt
@@ -0,0 +1,9 @@
+package com.example.reshme_nammapride.domain.model
+
+enum class InstarStage(val displayName: String) {
+    FIRST_INSTAR("1st Instar (Chawki)"),
+    SECOND_INSTAR("2nd Instar (Chawki)"),
+    THIRD_INSTAR("3rd Instar"),
+    FOURTH_INSTAR("4th Instar"),
+    FIFTH_INSTAR("5th Instar (Final)")
+}
