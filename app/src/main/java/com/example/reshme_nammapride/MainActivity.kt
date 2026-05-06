a7b8773f15b0aaee45471ab324faf812beb559ec
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-06 13:07:42 +0530

    feat: define main activity

--- app/src/main/java/com/example/reshme_nammapride/MainActivity.kt
+++ app/src/main/java/com/example/reshme_nammapride/MainActivity.kt
@@ -3,45 +3,46 @@ package com.example.reshme_nammapride
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
+import com.example.reshme_nammapride.data.local.AppDatabase
+import com.example.reshme_nammapride.ui.screens.entry.EntryScreen
 import com.example.reshme_nammapride.ui.theme.ReshmeNammaPrideTheme
+import com.example.reshme_nammapride.viewmodel.ClimateViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // initialize Database
+        val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java, "reshme_database"
+            ).fallbackToDestructiveMigration(false).build()
+
+        val rearingDao = db.rearingDao()
+
         setContent {
             ReshmeNammaPrideTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface {
+                    // Create viewModel
+                    val climateViewModel: ClimateViewModel = viewModel(
+                        factory = object : ViewModelProvider.Factory {
+                            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                                return ClimateViewModel(rearingDao) as T
+                            }
+                        }
                     )
+
+                    EntryScreen(viewModel = climateViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReshmeNammaPrideTheme {
-        Greeting("Android!")
-    }
-}
