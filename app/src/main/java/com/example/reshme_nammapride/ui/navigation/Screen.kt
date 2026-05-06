7ba48a6d698a6da6d71377d8d7c2e8e11eb34a1c
Chandana Ramesh <chandanaramesh516@gmail.com>
2026-05-06 19:48:03 +0530

    feat: add new route for archive

--- app/src/main/java/com/example/reshme_nammapride/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/reshme_nammapride/ui/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.example.reshme_nammapride.ui.navigation
 sealed class Screen(val route: String) {
     object Entry : Screen("entry")
     object History : Screen("history")
+    object Archive : Screen("archive")
 }
